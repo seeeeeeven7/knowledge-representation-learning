46f30c3c3fa2abbcfd1ff005ab3f31368fefd4aa
Steven Hu <sourcedark@gmail.com>
2018-01-16 19:38:51 +0800

    try evaluate model

--- models/TransE/train.py
+++ models/TransE/train.py
@@ -11,7 +11,7 @@ MARGIN = 1.0
 # Load data
 
 with open('../../data/WN18/wordnet-mlj12-train.txt', 'r') as trainfile:
-	triples = trainfile.read().splitlines()
+    triples = trainfile.read().splitlines()
 
 triples = list(map(lambda x : x.split('\t'), triples))
 
@@ -19,14 +19,15 @@ entity_index = {}
 relation_index = {}
 
 for triple in triples:
-	if triple[0] not in entity_index:
-		entity_index[triple[0]] = len(entity_index)
-	if triple[2] not in entity_index:
-		entity_index[triple[2]] = len(entity_index)
-	if triple[1] not in relation_index:
-		relation_index[triple[1]] = len(relation_index)
+    if triple[0] not in entity_index:
+        entity_index[triple[0]] = len(entity_index)
+    if triple[2] not in entity_index:
+        entity_index[triple[2]] = len(entity_index)
+    if triple[1] not in relation_index:
+        relation_index[triple[1]] = len(relation_index)
 
 print(len(triples))
+print(len(entity_index))
 
 # Variable
 ERs = tf.Variable(tf.random_uniform([len(entity_index), D], -6 / D, 6 / D, dtype=tf.float32))
@@ -57,32 +58,34 @@ sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
 # Evaluation Model
-Z = tf.placeholder(tf.int64, [None, ])
-tf.concat(tf.expand_dims(X[:,1:], 0), tf.reshape()
+x = tf.placeholder(tf.int64, [3])
+HR = tf.nn.l2_normalize(tf.nn.embedding_lookup(ERs, x[0]), 0) # [300]
+RR = tf.nn.embedding_lookup(RRs, x[1]) # [300]
+TR = tf.nn.l2_normalize(tf.nn.embedding_lookup(ERs, x[2]), 0) # [300]
+rankH = tf.count_nonzero(tf.nn.relu(-tf.norm(tf.nn.l2_normalize(ERs, 1) - (TR - RR), axis = 1) + tf.norm(TR - RR - HR)))
 
 # Train
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 BATCH_INDEX = 0
 while True:
-	batch_xs = []
-	batch_ys = []
-	for __ in range(BATCH_SIZE):
-		index = numpy.random.randint(len(triples))
-		batch_xs.append([entity_index[triples[index][0]], relation_index[triples[index][1]], entity_index[triples[index][2]]])
-		if numpy.random.randint(2) == 0:
-			batch_ys.append([entity_index[triples[index][0]], relation_index[triples[index][1]], numpy.random.randint(len(entity_index))])
-		else:
-			batch_ys.append([numpy.random.randint(len(entity_index)), relation_index[triples[index][1]], entity_index[triples[index][2]]])
-	sess.run(train_step, feed_dict={X: batch_xs, Y:batch_ys})
+    batch_xs = []
+    batch_ys = []
+    for __ in range(BATCH_SIZE):
+        index = numpy.random.randint(len(triples))
+        batch_xs.append([entity_index[triples[index][0]], relation_index[triples[index][1]], entity_index[triples[index][2]]])
+        if numpy.random.randint(2) == 0:
+            batch_ys.append([entity_index[triples[index][0]], relation_index[triples[index][1]], numpy.random.randint(len(entity_index))])
+        else:
+            batch_ys.append([numpy.random.randint(len(entity_index)), relation_index[triples[index][1]], entity_index[triples[index][2]]])
+    sess.run(train_step, feed_dict={X: batch_xs, Y:batch_ys})
 
-	if BATCH_INDEX % 100 == 0:
-		print(BATCH_INDEX, sess.run(loss, feed_dict={X: batch_xs, Y:batch_ys}))
-		print(ERs[0].eval()   )
+    if BATCH_INDEX % 100 == 0:
+        print(BATCH_INDEX, sess.run(loss, feed_dict={X: batch_xs, Y:batch_ys}))
 
-	if BATCH_INDEX % 1000 == 0:
-		for triple in triples:
-			# Predict head
-			for h, hv in entity_index.items():
-				pass
+    if BATCH_INDEX % 1000 == 0:
+        rankSum = 0
+        for index in range(20):
+            rankSum = rankSum + sess.run(rankH, feed_dict={x: batch_xs[index]})
+        print(rankSum / 20)
 
-	BATCH_INDEX = BATCH_INDEX + 1
+    BATCH_INDEX = BATCH_INDEX + 1
